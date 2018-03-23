f24de42a022fa35bc57b02d16d3bc89d02490e8f
Elton Memishaj <eltoni.memishi@live.com>
2018-03-23 17:56:13 +0100

    Added background and custom cloud and side-nav.

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    openSideNav();
+});
+function fabOnClick(fab, currentColor, nextColor) {
+    var icon = fab.children[0];
+    if(icon.innerHTML == 'create') {
+        icon.innerHTML = 'close';
+        fab.style['background-color'] = nextColor;
+        icon.style.transform = 'rotateZ(90deg)';
+    }
+    else {
+        icon.innerHTML = 'create';
+        fab.style['background-color'] = currentColor;
+        icon.style.transform = 'rotateZ(0deg)';
+    }
+}
+function openSideNav() {
+    hamburger.innerHTML = 'arrow_forward';
+    hamburger.style.transform = 'rotateZ(180deg)';
+    document.getElementById('side-nav').style.transform = 'translateX(0)';
+}
+function closeSideNav() {
+    hamburger.innerHTML = 'menu';
+    hamburger.style.transform = 'rotateZ(0deg)';
+    document.getElementById('side-nav').style.transform = 'translateX(-100%)';
+}
